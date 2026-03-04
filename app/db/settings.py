ceb8f18fc6d20a9f6abf1ffa8749359726272f84
SiokhMatviu <siokhmatviu@gmail.com>
2026-03-04 19:59:33 +0200

    fix .gitignore

---
+++ app/db/settings.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+env_path = Path(__file__).parent.parent / ".env"
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+
+    @property
+    def database_url(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+    model_config = SettingsConfigDict(env_file=env_path)
+
+
+settings = Settings()
